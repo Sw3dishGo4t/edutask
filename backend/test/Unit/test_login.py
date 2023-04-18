5ae37cab12e4930ffc8a6c7236967bb4fd20ac06
Sw3dishGo4t <anton.linell@hotmail.se>
2023-04-18 16:36:24 +0200

    Update test_login.py

--- backend/test/Unit/test_login.py
+++ backend/test/Unit/test_login.py
@@ -2,10 +2,14 @@ import pytest
 from src.controllers.usercontroller import UserController
 
 def test_getUser1():
+    email = "test.test@gmail.com"
     assert True == True
 def test_getUser2():
+    email = "test.test"
     assert True == True
 def test_getUser3():
+    email = "test.test@gmail.com"
     assert True == True
 def test_getUser4():
+    email = "test.test"
     assert True == True
