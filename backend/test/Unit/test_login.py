fdc83407cf3c4093fd00d2bd626446a031111b3d
Sw3dishGo4t <anton.linell@hotmail.se>
2023-04-18 16:29:46 +0200

    Create test_login.py

---
+++ backend/test/Unit/test_login.py
@@ -0,0 +1,11 @@
+import pytest
+from src.controllers.usercontroller import UserController
+
+def test_getUser1():
+    assert True == True
+def test_getUser2():
+    assert True == True
+def test_getUser3():
+    assert True == True
+def test_getUser4():
+    assert True == True
