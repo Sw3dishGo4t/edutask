4e829a8a1cf6e750d9ddddbec662e22ccbec9d82
Sw3dishGo4t <anton.linell@hotmail.se>
2023-04-24 14:29:16 +0200

    Update test_login.py

--- backend/test/Unit/test_login.py
+++ backend/test/Unit/test_login.py
@@ -4,9 +4,11 @@ from src.controllers.usercontroller import UserController
 
 def test_getUser1():
     mockdao = mock.MagicMock()
-    mockdao.
+    mockdao.find.return_value = {'users': "TestUser"}
+    sut = UserController
     email = "test.test@gmail.com"
-    assert True == True
+    
+    assert sut.get_user_by_email(email) == "TestUser"
 def test_getUser2():
     email = "test.test"
     assert True == True
