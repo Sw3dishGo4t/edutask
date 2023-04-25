55c1a7c7033bd85b8f91bfb0f6b653954083543b
Sw3dishGo4t <anton.linell@hotmail.se>
2023-04-25 14:46:52 +0200

    Update test_create.py

--- backend/test/Integration/test_create.py
+++ backend/test/Integration/test_create.py
@@ -1,3 +1,31 @@
 import pytest
 import unittest.mock as mock
 from src.util.dao import DAO
+
+@pytest.fixture
+def sut():
+    temp = 2
+
+def test_create_document_1(sut):
+    assert True == True
+
+def test_create_document_2(sut):
+    assert True == True
+
+def test_create_document_3(sut):
+    assert True == True
+
+def test_create_document_4(sut):
+    assert True == True
+
+def test_create_document_5(sut):
+    assert True == True
+
+def test_create_document_6(sut):
+    assert True == True
+
+def test_create_document_7(sut):
+    assert True == True
+
+def test_create_document_8(sut):
+    assert True == True
