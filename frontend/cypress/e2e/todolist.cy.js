9356d9f1ea0b044ca1fdceb3b48ec9fdaa3c358f
Sw3dishGo4t <anton.linell@hotmail.se>
2023-04-28 13:19:32 +0200

    Update todolist.cy.js

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Add new todo item', () => {
+  before(function(){
+    
+  })
+
+  it('add button is disabled', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('add button is enabled', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('todo item is added tp list', () => {
     cy.visit('https://example.cypress.io')
   })
 })
