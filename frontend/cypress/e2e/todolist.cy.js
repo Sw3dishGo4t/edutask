c8514fd3043387023538d0a4916a938e1e60d023
Sw3dishGo4t <anton.linell@hotmail.se>
2023-05-15 16:46:42 +0200

    add workflow CI

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -42,19 +42,105 @@ describe('Add new todo item', () => {
       .find('input[type=text]')
       .type('dQw4w9WgXcQ')
     
+    cy.get('form')
+      .submit()
   })
 
   it('add button is disabled', () => {
-    
+    cy.get(':nth-child(2) > a ')
+      .click()
+
+    cy.get('.inline-form > [type="submit"]')
+      .should('be.disabled')
   })
 
   it('add button is enabled', () => {
+    cy.get(':nth-child(2) > a ')
+      .click()
+
+    cy.get('.inline-form > [type="text"]')
+      .type('Play on repeat')
     
+    cy.get('.inline-form > [type="submit"]')
+      .should('be.enabled')
   })
 
-  it('todo item is added tp list', () => {
+  it('todo item is added to list', () => {
+    cy.get(':nth-child(2) > a ')
+    .click()
+
+    cy.get('.inline-form > [type="text"]')
+      .type('Play on repeat')
     
+    cy.get('.inline-form > [type="submit"]')
+      .click()
+  
+    cy.get('.todo-list > :nth-child(2) > .editable')
+      .should('contain.text', 'Play on repeat')
   })
+
+  it('todo item gets deactivated', () => {
+    cy.get(':nth-child(2) > a ')
+    .click()
+
+    cy.get('.inline-form > [type="text"]')
+      .type('Play on repeat')
+    
+    cy.get('.inline-form > [type="submit"]')
+      .click()
+  
+    cy.get('.todo-list > :nth-child(2) > .checker')
+      .click()
+    cy.get('.todo-list > :nth-child(2) > .checker')
+      .should('have.class', 'checker')
+  })
+
+  it('todo item gets activated', () => {
+    cy.get(':nth-child(2) > a ')
+    .click()
+
+    cy.get('.inline-form > [type="text"]')
+      .type('Play on repeat')
+    
+    cy.get('.inline-form > [type="submit"]')
+      .click()
+  
+    cy.get('.todo-list > :nth-child(2) > .checker')
+      .click()
+      .click()
+    
+    cy.get('.todo-list > :nth-child(2) > .checker')
+      .should('contain.text', '')
+  })
+
+  it('todo item gets deleated', () => {
+    cy.get(':nth-child(2) > a ')
+    .click()
+
+    cy.get('.inline-form > [type="text"]')
+      .type('Play on repeat')
+    
+    cy.get('.inline-form > [type="submit"]')
+      .click()
+  
+    cy.get(':nth-child(2) > .remover')
+      .click()
+
+    cy.get(':nth-child(2)')
+      .should()
+  })
+
+
+  /*afterEach(function(){
+      // clean up by deleting the user from the database
+      cy.request({
+        method: 'DELETE',
+        url: `http://localhost:5000/tasks/ofuser/${uid}`
+      }).then((response) => {
+        cy.log(response.body)
+      })
+  })*/
+
   after(function () {
     // clean up by deleting the user from the database
     cy.request({
